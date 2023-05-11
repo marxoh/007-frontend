731a3fb6085b9257af30bbd0cf5f9a5cf7e418c5
Marcos Mayorga <marxoh22@gmail.com>
2023-05-11 14:07:07 -0400

    refreshControlOK

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,14 +1,37 @@
-// lista de tareas que vamos a obtener del backend
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+//lista de tareas que vamos a obtener del backend
+//flatlist: lista de elementos que recorre automaticamente
+import { FlatList, RefreshControl } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getTasks } from "../api";
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks }) => {
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadTasks = async () => {
+    //de getTasks() viene un json que contiene los datos de la api
+    const data = await getTasks();
+    // console.log(data);
+    setTasks(data);
+  };
+
+  //al cargar la pagina, como el componentdidmount
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
   //{item}: con llaves solo para extraer el item del obj que llega
   const renderItem = ({ item }) => {
     //se va a querer retornar un componente TaskItem
     return <TaskItem task={item} />;
   };
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    await loadTasks();
+    setRefreshing(false);
+  });
 
   return (
     //FlatList: contenedor de cada uno de los items
@@ -21,6 +44,14 @@ const TaskList = ({ tasks }) => {
       keyExtractor={(item) => item.id + ""}
       //renderiza el elemento de la lista
       renderItem={renderItem}
+      //refreshControl:
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          colors={["#78408f"]}
+          onRefresh={onRefresh}
+        />
+      }
     />
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,32 +1,13 @@
-// pantalla inicial
-//flatlist: lista de elementos que recorre automaticamente
-import { View } from "react-native";
-import React, { useEffect, useState } from "react";
-import { getTasks } from "../api";
+// pantalla inicial con la lista de tareas
+import React from "react";
 import Layout from "../components/Layout";
 import TaskList from "../components/TaskList";
 
-const HomeScreen = () => {
-  const [tasks, setTasks] = useState([]);
-
-  const loadTasks = async () => {
-    //de getTasks() viene un json que contiene los datos de la api
-    const data = await getTasks();
-    // console.log(data);
-    setTasks(data);
-  };
-
-  //al cargar la pagina, como el componentdidmount
-  useEffect(() => {
-    loadTasks();
-  }, []);
-
-  return (
-    //se pone el children de Layout
-    <Layout>
-      <TaskList tasks={tasks} />
-    </Layout>
-  )
-};
+const HomeScreen = () => (
+  //se pone el children de Layout
+  <Layout>
+    <TaskList />
+  </Layout>
+);
 
 export default HomeScreen;
